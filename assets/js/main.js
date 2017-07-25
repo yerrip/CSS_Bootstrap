707ebae091d68eae690d04b84dd179034ac5f320
Yerri Portillo <yportill@lesley.edu>
2017-07-24 22:36:01 -0400

    JS Issue fixed and dropdown added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-$(".navbar-toggle").on("click", function() {
-
-  console.log(5 + 6);
-
-$("#collapseList").toggleClass('show')
-})
+// $(".navbar-toggle").on("click", function() {
+//
+//   console.log(5 + 6);
+//
+// $("#collapseList").toggleClass('show')
+// })
